78c92ac0f383c59cf6330b74b5089e7eb9a29fb8
Alejandra Paulina Pérez González <82723700+paulinapglz99@users.noreply.github.com>
2023-10-28 16:50:31 -0600

    Add files via upload

--- 1.script_mat_coexpre_rosmap.R
+++ 1.script_mat_coexpre_rosmap.R
@@ -1,32 +1,32 @@
-#Este script toma datos normalizados en FPKM de RNAseq y calcula una matriz de coexpresion
-#Con esta matriz de coexpresion se realiza mas adelante la matriz de informacion mutua
+#This script takes FPKM normalized data from RNAseq and calculates a coexpression matrix.
+#With this coexpression matrix, the mutual information matrix is performed later on next block
 
-#paquetes
+#Libraries
 
 pacman::p_load("tidyverse", 
        "ggplot2", 
        'vroom', 
        'biomaRt')
 
-##abrir archivos
+#Read data
 
-dir<-'FPKM_AD.csv' #archivo
+dir<-'FPKM_AD.csv' #discretized coexpression matrix
 
 FPKM <- vroom(file = dir)
 
-##join tablas con datos de expresión diferentes plates SI ES NECESARIO
+##join tables with different expression data plates IF NEEDED
 
-#expre_junto<-dplyr::left_join(x=genes_expre_p1_p6,
+#FPKM <-dplyr::left_join(x=genes_expre_p1_p6,
 #                              y=genes_expre_p7_p8,
 #                             by=c('gene_id'='gene_id'))
 #
-#expre_junto$tracking_id.x=NULL
+#FPKM$tracking_id.x=NULL
 
-#vroom_write(expre_junto, 
+#vroom_write(FPKM, 
 #           file = 'RNAseq_FPKM_1_to_8_merged.csv', 
 #           delim = ',')
 
-##Limpiar columna 'gene_id': quitar los puntos del gene_id (tomo parte del código de sol)
+##Clean column 'gene_id': remove .XX from gene_id
 
 identificadores<- pull(FPKM,gene_id)
 
@@ -34,8 +34,7 @@ identificadores<-sapply(strsplit(identificadores,".",fixed=T),function(x) x[1])
 
 FPKM <- FPKM %>% add_column(identificadores)
 
-
-#Generar anotacion con ensembl. Annotate gene_biotype, GC content
+#Generate annotation with ensembl. Annotate gene_biotype, GC content
 
 mart <- useEnsembl("ensembl",
                    dataset="hsapiens_gene_ensembl")
@@ -47,13 +46,13 @@ myannot <- getBM(attributes = c("ensembl_gene_id",
                  values= identificadores,
                  mart= mart)
 
-##juntar anotaciones de biomart con tabla de datos, para generar la data anotada
+##join biomart annotations with data table, to generate the annotated data
 
 expre <-left_join(x = FPKM,
                   y = myannot,
                   by = c('identificadores'='ensembl_gene_id'))
 
-##Quedarme solo con los datos de tipo 'gene coding' (pues por ahora son los únicos que nos interesan)
+##Keep only the 'gene coding' type data (since for now they are the only ones we are interested in).
 
 protcod <- filter(expre, 
                        gene_biotype =='protein_coding')
@@ -64,13 +63,13 @@ valores_expre <- protcod %>%
                 -identificadores,
                 -percentage_gene_gc_content)
 
-####  Discretizacion de los datos
+#### Discretization of data
 mat_dis<-infotheo::discretize(t(valores_expre))
 
 #Save matrix
 
-vroom_write(mat_dis, 
-        file = 'coexpression_matrix_AD.txt', 
-       delim = ',')
+#vroom_write(mat_dis, 
+#       file = 'coexpression_matrix_AD.txt', 
+#      delim = ',')
 
-###De aqui el siguiente sript es script_calculo_mi_parallel.R
+###Next script is 3.mutualinformation_parallel.R
