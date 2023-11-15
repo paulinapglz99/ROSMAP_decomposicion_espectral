a9c5c26925c9dfe51c8de34e828d631bf8344f12
Alejandra Paulina Pérez González <82723700+paulinapglz99@users.noreply.github.com>
2023-11-14 21:32:11 -0600

    Update 0.Handle_metadata.R

--- 0.Handle_metadata.R
+++ 0.Handle_metadata.R
@@ -31,3 +31,20 @@ FPKM_p7_p8 <- vroom(file = 'ROSMAP_RNAseq_FPKM_gene_plates_7_to_8_normalized.tsv
 FPKM_p1_p8<-left_join(x=FPKM_p1_p6,
                       y=FPKM_p7_p8,
                       by=c('gene_id'='gene_id'))
+
+#Match Clinical.csv file to RNA Seq files and subset -----------------------
+
+#Here we handle with inopportune data like the copy of the gene ID col
+#and two repeated samples with _6 and _7 are deleted (_0 was left)
+
+#merge both plates
+
+FPKM_p1_p8 <- FPKM_p1_p8 %>% 
+  dplyr::select(-c("tracking_id.x", "tracking_id.y", #remove inopportune data 
+            "492_120515_6", "492_120515_7"))
+
+#save table for later
+
+#vroom_write(FPKM_p1_p8,
+#           file = "RNAseq_FPKM_1_to_8_merged.csv", 
+#           delim = ",")
