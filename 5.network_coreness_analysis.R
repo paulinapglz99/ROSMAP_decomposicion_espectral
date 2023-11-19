ddd66319692236720e1bd67ac25594d509b4dd2b
Alejandra Paulina Pérez González <82723700+paulinapglz99@users.noreply.github.com>
2023-11-18 22:09:52 -0600

    Update 5.network_coreness_analysis.R

--- 5.network_coreness_analysis.R
+++ 5.network_coreness_analysis.R
@@ -22,24 +22,18 @@ rownames(coreness) <- NULL
 
 #Histogram of the coreness distribution
 
-hist(coreness$core_by_node)
-
-max(coreness$core_by_node) #el maximo de coreness por nodo para este grafo
-
 hist_coreness <- ggplot(coreness, aes(x = core_by_node)) +
   geom_histogram(fill = "skyblue", color = "white") +
   labs(title = "Histograma de coreness por nodo",
        subtitle = "Para sujetos ROSMAP sin demencia,despues del corte de percolacion de MI",
        x = "Coreness by node", y = "Frecuencia") +
-  scale_x_continuous(breaks = seq(0, 160, by = 10)) +
+  scale_x_continuous(breaks = seq(0, max(coreness$core_by_node), by = 10)) +
   theme_light()
 
 # Filtering by coreness -----
 
 coreness_filter <- coreness %>% 
   filter(core_by_node >= 20) #filtrar para tener un grafo con nodos con un core max de __
-
-hist(coreness_filter$core_by_node)
 
 #Histogram of filtered coreness----
 
@@ -48,16 +42,14 @@ hist_coreness_filter <- ggplot(coreness_filter, aes(x = core_by_node)) +
   labs(title = "Histograma de coreness por nodo con filtro",
        subtitle = "Para sujetos ROSMAP sin demencia, despues del corte de percolacion de MI",
        x = "Coreness by node", y = "Frecuencia") +
-  scale_x_continuous(breaks = seq(0, 160, by = 10)) +
+  scale_x_continuous(breaks = seq(0, max(coreness_filter$core_by_node), by = 10)) +
   theme_light()
 
 #Now the subgraph ----
 
 # Create a vector with the name of nodes I want to subset
 
-coreness_filter_v <- as.vector(coreness_filter$gene)
-
-coreness_filter_v <- unlist(coreness_filter_v)
+coreness_filter_v <- coreness_filter$gene
 
 #haciendo un subgrafo solo con los vertices del core we want
 
@@ -67,3 +59,42 @@ subgraph_kcore <- induced_subgraph(graph,
 #plotear 
 
 subraph_kcore_filter.p <- plot(subgraph_kcore)
+
+#write graphs in graphml ----
+
+#write_graph(subgraph_kcore, 
+#           file = 'coexpre_noMCI_18112023_graph_MI0.5_coreness_20.graphml', 
+#           'graphml') #'edgelist.txt' / '.graphml'
+
+#Automatize to have other kcore graphs -----
+
+#####################################################################################################
+
+# Function to filter and save the subgraph
+
+coreness_maker <- function(coreness, graph, n) {
+  
+  # Filter by the value of core_by_node
+  coreness_filter <- coreness %>% 
+    filter(core_by_node >= n)
+  
+  # Obtain the vector with the names of nodes
+  coreness_filter_v <- coreness_filter$gene
+  
+  # Create the subgraph
+  subgraph_kcore <- induced_subgraph(graph, vids = coreness_filter_v)
+  
+  # Write the network in graphml format
+  filename <- paste0('coexpre_noMCI_18112023_graph_MI0.5_coreness_', n, '.graphml')
+  write_graph(subgraph_kcore,
+              file = filename, 
+              format = 'graphml')
+}
+
+# 'coreness' and 'graph' are initial data
+
+# Iterate over core_by_node values from 25 to 145 with increments of 10
+
+for (i in seq(25, 145, by = 10)) {
+  coreness_maker(coreness, graph, i)
+}
