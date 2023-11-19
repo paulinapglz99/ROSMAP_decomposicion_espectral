782ea951a3e17f64f3caef644f5c0c58a6fa2d57
Alejandra Paulina Pérez González <82723700+paulinapglz99@users.noreply.github.com>
2023-11-18 20:55:31 -0600

    Update 5.network_coreness_analysis.R

--- 5.network_coreness_analysis.R
+++ 5.network_coreness_analysis.R
@@ -13,51 +13,57 @@ graph <- read.graph(file = "noMCI_17112023_graph_MI0.532.graphml",
 #coreness ----
 
 coreness <- coreness(graph) %>% 
-  as.data.frame()
+  as.data.frame() %>% 
+  rename("core_by_node" = ".") 
+
+coreness$gene <- rownames(coreness)
+
+rownames(coreness) <- NULL
 
 #Histogram of the coreness distribution
 
-hist(coreness$.)
+hist(coreness$core_by_node)
+
+max(coreness$core_by_node) #el maximo de coreness por nodo para este grafo
+
+hist_coreness <- ggplot(coreness, aes(x = core_by_node)) +
+  geom_histogram(fill = "skyblue", color = "white") +
+  labs(title = "Histograma de coreness por nodo",
+       subtitle = "Para sujetos ROSMAP sin demencia,despues del corte de percolacion de MI",
+       x = "Coreness by node", y = "Frecuencia") +
+  scale_x_continuous(breaks = seq(0, 160, by = 10)) +
+  theme_light()
 
 # Filtering by coreness -----
 
-coreness_25 <- coreness %>% 
-  filter(. >= 25) #filtrar para tener un grafo con nodos con un core max de 25
+coreness_filter <- coreness %>% 
+  filter(core_by_node >= 20) #filtrar para tener un grafo con nodos con un core max de __
 
-hist(coreness_25$.)
+hist(coreness_filter$core_by_node)
+
+#Histogram of filtered coreness----
+
+hist_coreness_filter <- ggplot(coreness_filter, aes(x = core_by_node)) +
+  geom_histogram(fill = "skyblue", color = "white") +
+  labs(title = "Histograma de coreness por nodo con filtro",
+       subtitle = "Para sujetos ROSMAP sin demencia, despues del corte de percolacion de MI",
+       x = "Coreness by node", y = "Frecuencia") +
+  scale_x_continuous(breaks = seq(0, 160, by = 10)) +
+  theme_light()
 
 #Now the subgraph ----
 
 # Create a vector with the name of nodes I want to subset
 
-coreness_25v <- as.vector(rownames(coreness_25))
+coreness_filter_v <- as.vector(coreness_filter$gene)
 
-#haciendo un subgrafo solo con los vertices de coreness_3
+coreness_filter_v <- unlist(coreness_filter_v)
 
-subraph_kcore3 <- induced_subgraph(graph, vids = coreness_25)
+#haciendo un subgrafo solo con los vertices del core we want
+
+subgraph_kcore <- induced_subgraph(graph,
+                                    vids = coreness_filter_v)
 
 #plotear 
 
-print(subraph_kcore3)
-subraph_kcore3.p <- plot(subraph_kcore3) #IFUCKINGDIDIT
-save.image()
-
-
-###sin plotear para que no muera esta cosa
-#hacer red a partir de data frame
-
-graph_complete <- graph_from_data_frame(matrix, 
-                               directed = F)  #por fin veo una red :'u
-#coreness ----
-
-coreness <- coreness(graph_complete) %>% 
-  as.data.frame()
-
-coreness_3 <- coreness %>% 
-  filter(. == 3) #filtrar para tener solo esos cores
-
-coreness_3 <- as.vector(rownames(coreness_3))
-
-#haciendo un subgrafo solo con los vertices de coreness_3
-
-subraph_kcore3 <- induced_subgraph(graph, vids = coreness_3)
+subraph_kcore_filter.p <- plot(subgraph_kcore)
