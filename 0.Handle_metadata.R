ffd59718df754f5a4c9aff6260b63a9976e70360
Alejandra Paulina Pérez González <82723700+paulinapglz99@users.noreply.github.com>
2024-01-10 12:50:14 -0600

    Update 0.Handle_metadata.R

--- 0.Handle_metadata.R
+++ 0.Handle_metadata.R
@@ -1,14 +1,18 @@
+#Script 0.Handle_metadata.R
+#This pre-pre-process script reads ROSMAP metadata and FPKM counts data and handle both for further analysis
+#paulinapglz.99@gmail.com
+
 #libraries  ----- 
 
 pacman::p_load("dplyr")
 
-#Handle metadata ---------------
+#Read metadata ---------------
 
 clinical_metadata <- vroom::vroom(file = '/datos/rosmap/metadata/ROSMAP_clinical.csv')
                           
 biospecimen_metadata <- vroom::vroom(file = '/datos/rosmap/metadata/ROSMAP_biospecimen_metadata.csv')
 
-#merge metadata in one
+#merge metadata in one dataset
 
 cli_bio_metadata <- left_join(x = clinical_metadata, 
                               y = biospecimen_metadata, 
@@ -17,27 +21,23 @@ cli_bio_metadata <- left_join(x = clinical_metadata,
 #save table for later
 
 #vroom_write(cli_bio_metadata,
-#           file = "cli_bio_metadata.csv", 
+#           file = "/datos/rosmap/metadata/cli_bio_metadata.csv", 
 #           delim = ",")
 
-#read expression data
+#read expression data, there's 2 count archives ---------------
 
 FPKM_p1_p6 <- vroom::vroom(file = '/datos/rosmap/FPKM_data/ROSMAP_RNAseq_FPKM_gene_plates_1_to_6_normalized.tsv')
 
 FPKM_p7_p8 <- vroom::vroom(file = '/datos/rosmap/FPKM_data/ROSMAP_RNAseq_FPKM_gene_plates_7_to_8_normalized.tsv')
 
-#merge expression data
+#merge expression data into one
 
 FPKM_p1_p8<-left_join(x=FPKM_p1_p6,
                       y=FPKM_p7_p8,
                       by=c('gene_id'='gene_id'))
 
-#Match Clinical.csv file to RNA Seq files and subset -----------------------
-
 #Here we handle with inopportune data like the copy of the gene ID col
 #and two repeated samples with _6 and _7 are deleted (_0 was left)
-
-#merge both plates
 
 FPKM_p1_p8 <- FPKM_p1_p8 %>% 
   dplyr::select(-c("tracking_id.x", "tracking_id.y", #remove inopportune data 
@@ -48,7 +48,7 @@ FPKM_p1_p8 <- FPKM_p1_p8 %>%
 #save table for later
 
 #vroom_write(FPKM_p1_p8,
-#           file = "RNAseq_FPKM_1_to_8_merged.csv", 
+#           file = "/datos/rosmap/FPKM_data/RNAseq_FPKM_1_to_8_merged.csv", 
 #           delim = ",")
 
 #Next script is 1.MatchFPKMandClinicalMetadata.R
