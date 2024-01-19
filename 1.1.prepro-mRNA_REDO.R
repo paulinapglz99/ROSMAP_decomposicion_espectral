6e17431d631f14d44d246a519db2d8c2149d7764
Alejandra Paulina Pérez González <82723700+paulinapglz99@users.noreply.github.com>
2024-01-18 18:04:27 -0600

    Update 1.1.prepro-mRNA_REDO.R

--- 1.1.prepro-mRNA_REDO.R
+++ 1.1.prepro-mRNA_REDO.R
@@ -69,16 +69,17 @@ expression <- expression %>%
   filter(gene_biotype == "protein_coding" & hgnc_symbol!="") %>% #only rows where gene_biotype is "protein_coding" and hgnc_symbol is not an empty string 
       distinct(ensembl_gene_id, .keep_all = TRUE) # Keeps only unique rows based on the ensembl_gene_id column
 
+#Obtain new annotation after filtering
+
+myannot <- expression %>% 
+  dplyr::select(1:7)
+
 #Obtain counts 
 
 expression_counts <- expression %>% 
-  dplyr::select(ensembl_gene_id, 9:ncol(expression))      
+  dplyr::select(ensembl_gene_id, 8:ncol(expression))      
 dim(expression_counts)
-#[1] 18848   622
-
-#Obtain new annotation after filtering
-
-
+#[1] 18848   623
 
 #Obtain factors      
 
@@ -86,17 +87,138 @@ factors <- data.frame(
   "specimen_ID" = colnames(expression_counts)[-1],
   "group" = 1)
 dim(factors)
-
-#[1] 621   2 # this means 621 specimen_IDs and only one factor
-
+#[1] 622   2 # this means 621 specimen_IDs and only one factor
 
 #for the factor format
 #the order of the elements of the factor must coincide with the order of the samples (columns)
 # in the expression data provided. Row number in factor must match with number of cols in data ("FPKM_exprots"). 
 
-noiseqData <- readData(data = expression_counts, 
-                       gc = myannot["percentage_gene_gc_content",],  #%GC in myannot
-                       biotype = myannot["gene_biotype",],          #biotype
-                       factors = RNA_seq_metadata,                 #variables indicating the experimental group for each sample
-                       length =  myannot["length",])               #gene length
+noiseqData <- readData(data = expression_counts[-1], 
+                       gc = myannot$percentage_gene_gc_content,  #%GC in myannot
+                       biotype = myannot$gene_biotype,          #biotype
+                       factors = factors,                 #variables indicating the experimental group for each sample
+                       length =  myannot$length)               #gene length
 
+#1)check expression bias per subtype
+
+#Obtain the likely counts of genes, organized by subtype,
+#from the noiseqData object
+
+mycountsbio <- dat(noiseqData, 
+                   type =  "countsbio",
+                   norm = T, 
+                   factor = NULL)
+#Plots
+
+#patients with repeated measures
+png("CountsOri.png")
+explo.plot(mycountsbio, plottype = "boxplot", samples = 1:10)
+dev.off()
+
+#2)check for low count genes
+png("lowcountsOri.png")
+explo.plot(mycountsbio, plottype = "barplot", samples = 1:10)
+dev.off()
+
+#Histogram of row means
+
+png("lowCountThres.png")
+hist(rowMeans(cpm(FPKM_exprots,log=T)),
+     ylab="genes",
+     xlab="mean of log CPM",
+     col="gray")
+abline(v=0,col="red")
+dev.off()
+
+#3)check for transcript composition bias
+
+#each sample "s" is compared to a reference "r" (which can be arbitrarily chosen).
+#by computing M values=log2(counts = countsr). 
+
+#Confidence intervals for the M median is computed by bootstrapping.
+#If the median of M values for each comparison is not in the CI, the deviation
+# of the sample is significant, therefore, normalization is needed 
+
+mycd <- dat(noiseqData, type = "cd", norm = T) #slooooow
+
+#[1] "Warning: 110 features with 0 counts in all samples are to be removed for this analysis."
+#[1] "Reference sample is: 525_120515"
+
+#[1] "Diagnostic test: FAILED. Normalization is required to correct this bias."
+
+mycd_table <- table(mycd@dat)
+
+table(mycd@dat$DiagnosticTest[,  "Diagnostic Test"])
+
+#FAILED PASSED 
+#12    609 
+
+#Plot for Mvalues
+
+png("MvaluesOri.png")
+explo.plot(mycd,samples=sample(1:ncol(FPKM_exprots),10))
+dev.off()
+
+#4)check for length & GC bias
+
+#A cubic spline regression model is fitted. Both the model p-value and the coefficient
+# of determination (R2) are shown. If the model p-value is significant and R2 value is
+# high (more than 70%), the expression depends on the feature
+
+myGCcontent <- dat(noiseqData,
+                   k = 0, type = "GCbias",
+                   factor = "ceradsc")
+
+#[1] "Warning: 110 features with 0 counts in all samples are to be removed for this analysis."
+#[1] "GC content bias detection is to be computed for:"
+#[1] "1" "2" "3" "4"
+
+#Residuals:
+#  Min      1Q  Median      3Q     Max 
+#-9.5011 -1.5303 -0.0613  1.3300  6.4901 
+
+png("GCbiasOri.png",width=1000)
+explo.plot(myGCcontent,
+           samples = NULL,
+           toplot = "global")
+dev.off()
+
+#The GC-content of each gene does not change from sample to sample, so it can be expected to
+#have little effect on differential expression analyses to a first approximation
+
+mylengthbias <- dat(noiseqData, 
+                    k = 0,
+                    type = "lengthbias",
+                    factor = "ceradsc")
+
+
+#[1] "Warning: 110 features with 0 counts in all samples are to be removed for this analysis."
+#[1] "Length bias detection information is to be computed for:"
+#[1] "1" "2" "3" "4"
+
+#Residuals:
+# Min      1Q  Median      3Q     Max 
+#-29.152  -2.099  -0.478   1.663  83.628 
+
+#Plot length bias
+
+png("lengthbiasOri.png")
+explo.plot(mylengthbias, 
+           samples = NULL, 
+           toplot = "global")
+dev.off()
+
+#5) check for batch effect
+
+myPCA <- dat(noiseqData,
+             type = "PCA", 
+             norm = F,
+             logtransf = F)
+
+#Plot PCA
+
+png("PCA_Ori.png")
+explo.plot(myPCA, samples = c(1,2),
+           plottype = "scores",
+           factor = "ceradsc")
+dev.off()
