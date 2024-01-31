8a94edcdc79da8ef6ba586e279556c4414936512
paulinapglz99 <paulinapglz.99@gmail.com>
2024-01-31 03:15:01 +0000

    modified 1.MatchCountsandetc, code style

--- 1.MatchCountsandClinicalMetadata.R
+++ 1.MatchCountsandClinicalMetadata.R
@@ -18,10 +18,10 @@ colnames(counts)[1] <- "gene_id"  #as we remove the last two characters we have 
 
 #Simplify metadata to have only the RNAseq assays in brain  ------
 
-RNA_seq_metadata <- metadata %>% 
+metadata <- metadata %>% 
   filter(assay == 'rnaSeq', 
          organ == 'brain') %>% 
-  dplyr::select(assay, individualID, specimenID,
+  dplyr::select(individualID, specimenID, assay, 
                 msex, cogdx, ceradsc, braaksc) #I also added columns for the factors later
 
 # filter specific columns based on the values of "specimenID"
@@ -36,25 +36,11 @@ dim(counts_matrix)
 
 #Simplify metadata to maintain only specimenIDs in the count matrix
 
-RNA_seq_metadata <-  RNA_seq_metadata %>%
-  filter(specimenID %in% colnames(counts_matrix))
-dim(RNA_seq_metadata)
+metadata <- metadata %>%
+  filter(specimenID %in% colnames(counts_matrix))%>% 
+  filter(cogdx != 6)  #we don't need cogdx == 6, other types of dementia
+dim(metadata)
 #[1] 636   5
-
-#As the metadata dictionary says:
-##1 NCI: No cognitive impairment (No impaired domains)
-##2 MCI: Mild cognitive impairment (One impaired domain) and NO other cause of CI
-##3 MCI: Mild cognitive impairment (One impaired domain) AND another cause of CI
-##4 AD: Alzheimer’s dementia and NO other cause of CI (NINCDS PROB AD) 
-##5 AD: Alzheimer’s dementia AND another cause of CI (NINCDS POSS AD)
-##6 Other dementia: Other primary cause of dementia
-
-#we don't need cogdx == 6, so we filter the metadata
-
-RNA_seq_metadata <- RNA_seq_metadata %>% 
-  filter(cogdx != 6)         
-dim(RNA_seq_metadata)
-# [1] 624   7
 
 #Now we have only specimenIDs from cognitive diagnosis we are interested in
 #Finally we subset RNAseq count tables to have only specimenIDs with cogdx wanted  ----------
@@ -67,15 +53,19 @@ dim(counts_matrix)
 
 #Generate new variable based on ante & post mortem data ----------
 
-RNA_seq_metadata <- RNA_seq_metadata %>%
+metadata <- metadata %>%
   mutate(final_diagnostic = case_when(
-    cogdx == 1 & ceradsc == 4 ~ "NCI",
+    cogdx == 1 ~ "NCI",
     cogdx == 2 & (ceradsc == 1 | ceradsc == 2) ~ "AD",
     cogdx == 2 ~ "MCI",
     TRUE ~ NA_character_  # Handle no-specified cases
   ))
 
+#Count of dx
+
 dx_counts <- table(RNA_seq_metadata$final_diagnostic, useNA = "always") %>% as.data.frame()
+
+#Save data --- ---
 
 #Save filtered metadata
 
