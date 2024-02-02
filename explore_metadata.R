8bc04c3caaab20581fbba3996baf96cb33a9f15f
paulinapglz99 <paulinapglz.99@gmail.com>
2024-02-02 05:20:08 +0000

    trying to corplot

--- explore_metadata.R
+++ explore_metadata.R
@@ -1,1 +1,23 @@
 #Explore metadata
+
+pacman::p_load(corrplot, 
+              dplyr)
+
+#Read metadata
+
+metadata <- vroom::vroom(file = '/datos/rosmap/metadata/cli_bio_metadata.csv')  
+
+#Filter to make a corplot
+
+metadata <- metadata %>% 
+  filter(assay == 'rnaSeq', 
+         organ == 'brain') %>% 
+  dplyr::select("individualID", "specimenID","msex", "educ",                
+                "race", "spanish", "apoe_genotype", "age_at_visit_max",    
+                 "age_death", "braaksc", "ceradsc",
+                "cogdx", "dcfdx_lv")
+
+cor_metadata<- metadata[3:13]
+
+cor(cor_metadata)
+
