cf9fd7feaca4c64d18e5f158807f7790e3062963
paulinapglz99 <paulinapglz.99@gmail.com>
2024-02-05 23:18:59 +0000

    explorePCA2 is now the good one, it does PCA

--- explore_PCA2.R
+++ explore_PCA2.R
@@ -17,27 +17,32 @@ counts <- vroom::vroom(file = "/datos/rosmap/FPKM_data/filtered_FPKM_matrix_2501
 metadata <- vroom::vroom(file = "/datos/rosmap/metadata/cli_bio_metadata.csv")
 
 #Manage data --- ---
-#If your data is not already scaled, then do Steps 1 and 2, if you don't need it
-#skip to Step 3
+
+#Extract gene data to further managing
+
+gene_names <- counts$gene_id
 
 #Step 1. Centering the Data
 
-counts_centered <- scale(counts[-1], scale = F, center = T) 
+#If your data is not already scaled, scale(scale = T) if you don't need it
+#just center with scale(center = T)
 
-#Step 2. #2. Scale the data
+counts <- scale(counts[-1], scale = T, center = T) #omited first column as it contains only names
+dim(counts)
+#[1] 55889   624 genes, samples
 
-counts_scaled_centered <- scale(counts_centered, scale = T, center = F) 
+#This centering and scaling together has the effect of making the column standard 
+#deviations equal to 1. 
 
-#This scaling has the effect of making the column standard deviations equal to one
-
-apply(counts_scaled_centered, 2, sd)
+apply(counts, 2, sd)
 
 ############################PCA 
 
 #Step 3. Data Reduction
 
-mat <- as.matrix(counts_scaled_centered)
-rownames(mat) <- counts$gene_id  #Add rownames
+mat <- as.matrix(counts)
+rownames(mat) <- gene_names  #Add rownames
+dim(mat)
 
 #Calculate PCA
 
@@ -45,11 +50,33 @@ pca <- prcomp(t(mat)) #we t() to have samples in rows
 
 #Elbow (Scree plot)
 
-plot(pca)
+plot(pca, main = 'Scree plot', xlab = 'Principal Components from 1 to 10', 
+     col = '#B48EAE',bg = 'lightgray')
+
+#Scree table 
+
+# Calculate the cumulative percentage of variance
+cumulative_percentage <- cumsum(pca$sdev^2 / sum(pca$sdev^2) * 100)
+
+# Create a data frame with PC number and percentage of variance
+variance_table <- data.frame(
+  PC = 1:length(pca$sdev),
+  Variance_Percentage = pca$sdev^2 / sum(pca$sdev^2) * 100,
+  cumulative_Percentage = cumulative_percentage
+)
+
+#Note: The percentage of variance is calculated as the squared singular value
+#of each PC divided by the sum of squared singular values, multiplied by 100.
+
+variance_table_95 <- variance_table %>% 
+  filter(cumulative_percentage <= 95)
+
+#Save table
+#vroom::vroom_write(variance_table_95, file = "variance95.tsv")
 
 #Loading Plot
 
-barplot(pca$rotation[,1])
+barplot(pca$rotation[,1], col = '#B48EAE')
 
 #PCA to table
 
@@ -58,11 +85,27 @@ pca_df <- pca_df %>%
   as_tibble() %>% 
   left_join(metadata)
 
-#Plot PCA
+#Plot PCA scoreplot
 
 pca_df %>% 
-  filter(PC2>-0.5 & PC1 < 1000000) %>%  #trampeando
+  #filter(PC2>-0.5 & PC1 < 1000000) %>%  #trampeando
   ggplot() +
-  aes(x = PC1, y = PC2) +
+  aes(x = PC1, y = PC2, colour = as.factor(cogdx)) +
+  geom_point() +
+  geom_text(mapping = aes(label = specimenID))
+
+
+pca_df %>% 
+  #filter(PC2>-0.5 & PC1 < 1000000) %>%  #trampeando
+  ggplot() +
+  aes(x = PC1, y = PC3) +
+  geom_point() +
+  geom_text(mapping = aes(label = specimenID))
+
+
+pca_df %>% 
+  #filter(PC2>-0.5 & PC1 < 1000000) %>%  #trampeando
+  ggplot() +
+  aes(x = PC2, y = PC3) +
   geom_point() +
   geom_text(mapping = aes(label = specimenID))
