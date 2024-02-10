3ccbc411d88520f7e8d5e4c4b7d40e6ff8910e9e
paulinapglz99 <paulinapglz.99@gmail.com>
2024-02-10 02:36:28 +0000

    added dx variables NIAreagan and final_d to 1.x

--- 1.MatchCountsandClinicalMetadata.R
+++ 1.MatchCountsandClinicalMetadata.R
@@ -42,19 +42,28 @@ batches$batch <- gsub("_", "", batches$batch)
 metadata <- metadata %>%
   left_join(batches, by = "specimenID")
 
-#Add definite dx to metadata
+#Add other dx variants to metadata
+#NIA reagan was constructed as https://www.sciencedirect.com/science/article/pii/S0197458097000572?via%3Dihub and
+#https://www.radc.rush.edu/docs/var/detail.htm?category=Pathology&subcategory=Alzheimer%27s+disease&variable=niareagansc
 
 metadata <- metadata %>%
+  mutate(NIA_reagan_ADLikelihood = case_when(
+    ceradsc == 4 ~ "4",  #No AD (0)
+    (ceradsc == 3 & (braaksc == 1 | braaksc == 2)) ~ "3", #Low (0)
+    (ceradsc == 2 & (braaksc == 3 | braaksc == 4)) ~ "2", #Intermediate
+    (ceradsc == 1 & (braaksc == 5 | braaksc == 6)) ~ "1", #High
+    TRUE ~ NA_character_  # Handle no-specified cases
+  )) %>% 
+  mutate(dicho_NIA_reagan = case_when(
+    (NIA_reagan_ADLikelihood == 1 | NIA_reagan_ADLikelihood == 2) ~ "1", 
+    (NIA_reagan_ADLikelihood == 3 | NIA_reagan_ADLikelihood == 4) ~ "0"
+      )) %>% 
   mutate(final_diagnostic = case_when(
-    cogdx == 1 ~ "NCI",
-    cogdx == 2 & (ceradsc == 1 | ceradsc == 2) ~ "AD",
-    cogdx == 2 ~ "MCI",
+    cogdx == 1 & (NIA_reagan_ADLikelihood == 4) ~ "NCI",
+    cogdx == 2 & (NIA_reagan_ADLikelihood == 3) ~ "MCI",
+    cogdx == 4  & (NIA_reagan_ADLikelihood == 1 | NIA_reagan_ADLikelihood == 2) ~ "AD",
     TRUE ~ NA_character_  # Handle no-specified cases
   ))
-
-#Count of dx
-
-dx_counts <- table(RNA_seq_metadata$final_diagnostic, useNA = "always") %>% as.data.frame()
 
 #Fix count matrix column names to match it with metadata
 
@@ -75,13 +84,19 @@ metadata <- metadata %>%
   filter(specimenID %in% colnames(counts_matrix))%>% 
   filter(cogdx != 6)  #we don't need cogdx == 6, other types of dementia
 dim(metadata)
-#[1] 636   12
+#[1] 636   13
+
+#Count of dx
+
+NIA_reagan_counts <- table(metadata$NIA_reagan_ADLikelihood, useNA = "always") %>% as.data.frame()
+
+fdx_counts <- table(metadata$final_diagnostic, useNA = "always") %>% as.data.frame()
 
 #Finally we filter again RNAseq count matrix to have only specimenIDs with metadata and viceversa  --- ---
 
 counts_matrix <- counts_matrix %>%
   dplyr::select(all_of(metadata$specimenID)) %>% 
-  mutate(gene_id = counts_matrix$gene_id, .before = 1) #added gene names
+  mutate(gene_id = counts_matrix$gene_id, .before = 1) #added gene names to a first column 
 dim(counts_matrix)
 #[1] 55889   625
 
@@ -89,10 +104,10 @@ dim(counts_matrix)
 
 #Save filtered metadata
 
-vroom::vroom_write(metadata, file = '/datos/rosmap/metadata/RNA_seq_metadata_080224.csv')
+#vroom::vroom_write(metadata, file = '/datos/rosmap/metadata/RNA_seq_metadata_080224.csv')
 
 #Save count table
 
-vroom::vroom_write(counts_matrix, file = '/datos/rosmap/FPKM_data/filtered_FPKM_matrix_250124.csv')
+#vroom::vroom_write(counts_matrix, file = '/datos/rosmap/FPKM_data/filtered_FPKM_matrix_250124.csv')
 
 #next script 1.1.prepro-mRNA.R
