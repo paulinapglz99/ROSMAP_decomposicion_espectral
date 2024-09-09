f339fb3dce1b57280bf08bc777d6c967c6f05efc
paulinapglz99 <paulinapglz.99@gmail.com>
2024-09-09 21:31:27 +0000

    null model

--- null_model.R
+++ null_model.R
@@ -1,103 +1,115 @@
-# Example using igraph
-library(igraph, 
-        ggplot)
+#null_model.R
+#This script builds two null models for each graph, by randomizing the networks while preserving the degree distribution. 
+#Specifically, we rewired the edges of the original networks 1,000 times using a degree-preserving randomization algorithm. 
+pacman::p_load("igraph", 
+        "ggplot")
 
-# Assume graphAD and graphnoAD are the adjacency matrices for the two networks
-# Load or construct the networks
-
-graphAD <- read_graph(file = '/datos/rosmap/data_by_counts/ROSMAP_counts/counts_by_tissue/DLFPC/counts_by_NIA_Reagan/graphs_NIA_Reagan/ROSMAP_RNAseq_DLPFC_AD_MutualInfograph_percentile99.99.graphml', format = 'graphml')
-
-graphnoAD <- read_graph(file = '/datos/rosmap/data_by_counts/ROSMAP_counts/counts_by_tissue/DLFPC/counts_by_NIA_Reagan/graphs_NIA_Reagan/ROSMAP_RNAseq_DLPFC_noAD_MutualInfograph_percentile99.99.graphml', format = 'graphml')
+#Declare functions ----
 
 # Function to calculate various metrics
 calculate_metrics <- function(graph) {
   list(
     clustering = transitivity(graph, type = "global"),  # Global clustering coefficient
     avg_path_length = mean_distance(graph, directed = FALSE),  # Average path length
-    modularity = modularity(cluster_fast_greedy(graph)),  # Modularity of the network
-    degree_distribution = degree_distribution(graph)  # Degree distribution
+    modularity = modularity(cluster_infomap(graph)),  # Modularity of the network, using infomap
   )
 }
+
+#Function to randomize networks ----
+
+randomize_graph <- function(graph, num_randomizations, niter = 1000) {
+  random_graphs <- vector("list", num_randomizations)
+  for (i in 1:num_randomizations) {
+    random_graphs[[i]] <- rewire(graph, with = keeping_degseq(niter = niter))
+  }
+  return(random_graphs)
+}
+
+#Function to calculate metrics from ranzomized networks
+
+calculate_metrics_random_graphs <- function(graph_list) {
+  lapply(graph_list, calculate_metrics)
+}
+
+#Function to plot any metric -----
+
+plot_metric_distribution <- function(random_metrics, observed_metric, metric_name, graph_type) {
+  random_df <- data.frame(value = random_metrics, type = paste0("Random (", graph_type, ")")) # Df of random variables
+  observed_df <- data.frame(value = observed_metric, type = paste0("Observed (", graph_type, ")"))   # Observed value
+  combined_df <- rbind(random_df, observed_df)   # Combine data frames
+  # Plot
+  ggplot(random_df, aes(x = value)) +
+    geom_histogram(aes(y = ..density..), binwidth = 0.001, fill = "lightblue", color = "black", alpha = 0.7) +
+    geom_density(color = "blue", lwd = 1) +
+    geom_vline(aes(xintercept = observed_metric), color = "red", linetype = "dashed", size = 1.2) +
+    labs(title = paste("Null Model for", metric_name, "(", graph_type, ")"), 
+         x = metric_name, 
+         y = "Density") + 
+    theme_minimal()
+}
+
+#Read data ----
+
+graphAD <- read_graph(file = '/datos/rosmap/data_by_counts/ROSMAP_counts/counts_by_tissue/DLFPC/counts_by_NIA_Reagan/graphs_NIA_Reagan/ROSMAP_RNAseq_DLPFC_AD_MutualInfograph_percentile99.99.graphml', format = 'graphml')
+
+graphnoAD <- read_graph(file = '/datos/rosmap/data_by_counts/ROSMAP_counts/counts_by_tissue/DLFPC/counts_by_NIA_Reagan/graphs_NIA_Reagan/ROSMAP_RNAseq_DLPFC_noAD_MutualInfograph_percentile99.99.graphml', format = 'graphml')
 
 # Calculate metrics for your original networks
 metrics_AD <- calculate_metrics(graphAD)
 metrics_noAD <- calculate_metrics(graphnoAD)
 
-# Print metrics for reference
-print(metrics_AD)
-print(metrics_noAD)
-
-#Create a Null Model (Randomize Networks)
-
 # Number of random networks to generate
 num_randomizations <- 1000
 
-# Initialize lists to store metrics for random networks
-metrics_AD_random <- vector("list", num_randomizations)
-metrics_noAD_random <- vector("list", num_randomizations)
+#Randomize
 
-# Generate random networks and calculate metrics
-for (i in 1:num_randomizations) {
-  # Rewire the network while keeping the degree sequence intact
-  random_graph_AD <- rewire(graphAD, with = keeping_degseq(niter = 1000))
-  random_graph_noAD <- rewire(graphnoAD, with = keeping_degseq(niter = 1000))
-  
-  # Calculate metrics for the random networks
-  metrics_AD_random[[i]] <- calculate_metrics(random_graph_AD)
-  metrics_noAD_random[[i]] <- calculate_metrics(random_graph_noAD)
-}
+random_graphs_AD <- randomize_graph(graphAD, num_randomizations)
+random_graphs_noAD <- randomize_graph(graphnoAD, num_randomizations)
 
-# Extract clustering coefficients for the random networks (AD and noAD)
+# Calculate metrics for randomized graphs
+metrics_AD_random <- calculate_metrics_random_graphs(random_graphs_AD)
+metrics_noAD_random <- calculate_metrics_random_graphs(random_graphs_noAD)
+
+# Extract metrics -----
+
+# Extract clustering coefficient
 clustering_AD_random <- sapply(metrics_AD_random, function(x) x$clustering)
 clustering_noAD_random <- sapply(metrics_noAD_random, function(x) x$clustering)
 
-# Extract average path lengths for the random networks (AD and noAD)
-avg_path_length_AD_random <- sapply(metrics_AD_random, function(x) x$avg_path_length)
-avg_path_length_noAD_random <- sapply(metrics_noAD_random, function(x) x$avg_path_length)
+# Plot clustering coefficient distribution
+plot_metric_distribution(clustering_AD_random, metrics_AD$clustering, "Clustering Coefficient", "AD")
+plot_metric_distribution(clustering_noAD_random, metrics_noAD$clustering, "Clustering Coefficient", "noAD")
+
+# Extract average path length 
+
+path_length_AD_random <- sapply(metrics_AD_random, function(x) x$avg_path_length)
+path_length_noAD_random <- sapply(metrics_noAD_random, function(x) x$avg_path_length)
+
+# Plot clustering average path length
+plot_metric_distribution(path_length_AD_random, metrics_AD$avg_path_length, "Average path length", "AD")
+plot_metric_distribution(path_length_noAD_random, metrics_noAD$avg_path_length, "Average path length", "noAD")
 
 # Extract modularity for the random networks (AD and noAD)
 modularity_AD_random <- sapply(metrics_AD_random, function(x) x$modularity)
 modularity_noAD_random <- sapply(metrics_noAD_random, function(x) x$modularity)
 
-# Create a data frame for graphAD random clustering coefficients
-clustering_AD_df <- data.frame(clustering_coefficient = clustering_AD_random, 
-                               type = "Random (AD)")
+# Plot clustering average path length
+plot_metric_distribution(modularity_AD_random, metrics_AD$modularity, "Modularity", "AD")
+plot_metric_distribution(modularity_noAD_random, metrics_noAD$modularity, "Modularity", "noAD")
 
-# Add the observed value to the data frame (for overlaying in the plot)
-observed_AD_df <- data.frame(clustering_coefficient = metrics_AD$clustering, 
-                             type = "Observed (AD)")
+# Confidence invtervals
 
-# Combine the random and observed values into one data frame
-clustering_AD_combined_df <- rbind(clustering_AD_df, observed_AD_df)
+#If calculate the 2.5 and 97.5 percentile for the randomized network metrics, we generate a 95% confidence interval. 
+#If the observed metrics fall outside this interval, this will indicate that the observed metrics are not likely to be the product of chance.
 
-# Create a similar data frame for graphnoAD
-clustering_noAD_df <- data.frame(clustering_coefficient = clustering_noAD_random, 
-                                 type = "Random (noAD)")
+# Intervalo de confianza al 95% para clustering coefficient
+clustering_ci_AD <- quantile(clustering_AD_random, probs = c(0.025, 0.975))
+clustering_ci_noAD <- quantile(clustering_noAD_random, probs = c(0.025, 0.975))
 
-observed_noAD_df <- data.frame(clustering_coefficient = metrics_noAD$clustering, 
-                               type = "Observed (noAD)")
+# Verificar si el valor observado cae fuera del intervalo de confianza
+print(clustering_ci_AD)
+print(metrics_AD$clustering)
+print(clustering_ci_noAD)
+print(metrics_noAD$clustering)
 
-clustering_noAD_combined_df <- rbind(clustering_noAD_df, observed_noAD_df)
-
-# Plot distribution of clustering coefficients for AD graph
-
-clustering_AD.p <- ggplot(clustering_AD_df, aes(x = clustering_coefficient)) +
-  geom_histogram(aes(y = ..density..), binwidth = 0.001, fill = "lightblue", color = "black", alpha = 0.7) +
-  geom_density(color = "blue", lwd = 1) +  # Agrega una curva de densidad para mayor suavidad
-  geom_vline(aes(xintercept = metrics_AD$clustering), color = "red", linetype = "dashed", size = 1.2) +
-  labs(title = "Null Model for Clustering Coefficient (AD)", 
-       x = "Clustering Coefficient", 
-       y = "Density") + 
-  theme_minimal()
-
-# Plot for graphnoAD
-clustering_noAD.p <- ggplot(clustering_noAD_df, aes(x = clustering_coefficient)) +
-  geom_histogram(aes(y = ..density..), binwidth = 0.001, fill = "lightblue", color = "black", alpha = 0.7) +
-  geom_density(color = "blue", lwd = 1) +  # Agrega una curva de densidad para mayor suavidad
-  geom_vline(aes(xintercept = metrics_AD$clustering), color = "red", linetype = "dashed", size = 1.2) +
-  labs(title = "Null Model for Clustering Coefficient (AD)", 
-       x = "Clustering Coefficient", 
-       y = "Density") + 
-  theme_minimal()
-
-
+#END
