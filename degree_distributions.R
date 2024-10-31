71fd9e8a3a19a0559b5247961633f58845f602a9
paulinapglz99 <paulinapglz.99@gmail.com>
2024-10-31 23:35:01 +0000

    modified grob plots in degree dis

--- degree_distributions.R
+++ degree_distributions.R
@@ -13,9 +13,9 @@ set.seed(10)
 
 #Get data --- --- 
 
-graphAD <- read_graph(file = '/datos/rosmap/data_by_counts/ROSMAP_counts/counts_by_tissue/DLFPC/counts_by_NIA_Reagan/graphs_NIA_Reagan/ROSMAP_RNAseq_DLPFC_AD_MutualInfograph_percentile99.99.graphml', format = 'graphml')
+graphAD <- read_graph(file = '/datos/rosmap/data_by_counts/ROSMAP_counts/counts_by_tissue/DLFPC/counts_by_NIA_Reagan/graphs_NIA_Reagan/ROSMAP_RNAseq_DLPFC_AD_MutualInfograph_percentile99.99_trad.graphml', format = 'graphml')
 
-graphnoAD <- read_graph(file = '/datos/rosmap/data_by_counts/ROSMAP_counts/counts_by_tissue/DLFPC/counts_by_NIA_Reagan/graphs_NIA_Reagan/ROSMAP_RNAseq_DLPFC_noAD_MutualInfograph_percentile99.99.graphml', format = 'graphml')
+graphnoAD <- read_graph(file = '/datos/rosmap/data_by_counts/ROSMAP_counts/counts_by_tissue/DLFPC/counts_by_NIA_Reagan/graphs_NIA_Reagan/ROSMAP_RNAseq_DLPFC_noAD_MutualInfograph_percentile99.99_trad.graphml', format = 'graphml')
 
 #Save graphs in a list
 
@@ -36,14 +36,28 @@ ADdegree.df <- data.frame(gene = names(ADdegree), degree = ADdegree)
 ADdegree_freq <- table(ADdegree.df$degree) %>% as.data.frame()
 colnames(ADdegree_freq) <- c("degree", "Freq")
 ADdegree_freq$degree <- as.numeric(as.character(ADdegree_freq$degree))
-ADdegree_freq$Prob <- ADdegree_freq$Freq / sum(ADdegree_freq$Freq) # Frecuencia relativa
+ADdegree_freq$Prob <- ADdegree_freq$Freq / sum(ADdegree_freq$Freq) 
+ADdegree_freq <- ADdegree_freq[order(ADdegree_freq$degree, decreasing = F), ]
+ADdegree_freq$CumulativeDegree <- cumsum(ADdegree_freq$Freq)
+# Logarithm of the cumulative sum
+ADdegree_freq$logCumulativeDegree <- log10(ADdegree_freq$CumulativeDegree)
+ADdegree_freq$dx <- "AD"
+ADdegree_freq$degree <- as.numeric(ADdegree_freq$degree)
+
+#noAD
 
 noADdegree <- degree_distributions[["graphnoAD"]]
 noADdegree.df <- data.frame(gene = names(noADdegree), degree = noADdegree)
 noADdegree_freq <- table(noADdegree.df$degree) %>% as.data.frame()
 colnames(noADdegree_freq) <- c("degree", "Freq")
 noADdegree_freq$degree <- as.numeric(as.character(noADdegree_freq$degree))
-noADdegree_freq$Prob <- noADdegree_freq$Freq / sum(noADdegree_freq$Freq) # Frecuencia relativa
+noADdegree_freq$Prob <- noADdegree_freq$Freq / sum(noADdegree_freq$Freq)
+noADdegree_freq <- noADdegree_freq[order(noADdegree_freq$degree, decreasing = F), ]
+noADdegree_freq$CumulativeDegree <- cumsum(noADdegree_freq$Freq)
+# Logarithm of the cumulative sum
+noADdegree_freq$logCumulativeDegree <- log10(noADdegree_freq$CumulativeDegree)
+noADdegree_freq$dx<- "no AD"
+noADdegree_freq$degree <- as.numeric(noADdegree_freq$degree)
 
 # Find the max value of "Freq" in both networks
 max_freq_AD <- max(ADdegree_freq$Freq)
@@ -56,6 +70,9 @@ max_degree_noAD <- max(as.numeric(as.character(noADdegree_freq$degree)))
 # Define limits for make histograms comparable
 max_y <- max(max_freq_AD, max_freq_noAD)
 max_x <- max(max_degree_AD, max_degree_noAD)
+
+# Bind rows of both degre dis 
+dis <- rbind(ADdegree_freq, noADdegree_freq)
 
 ##### Histograms #####
 
@@ -136,17 +153,13 @@ log_grid <- grid.arrange(log_log_AD, log_log_noAD, ncol =2)
 
 #Superponed
 
-# Combinar ambos data.frames
-combined_degree_freq <- rbind(ADdegree_freq, noADdegree_freq)
-
 # Crear el gráfico combinado
-log_log_combined <- ggplot(combined_degree_freq, aes(x = degree, y = Prob, color = dx)) +
+log_log_combined <- ggplot(dis, aes(x = degree, y = Prob, color = dx)) +
   geom_point(size = 2, alpha = 0.8) +  # Puntos más grandes y semitransparentes
   scale_x_log10() +  # Escala logarítmica en el eje x
   scale_y_log10() +  # Escala logarítmica en el eje y
   labs(x = expression("log<k>"), y = expression(logp(k)), 
-       title = " ", 
-       subtitle = "a)") +
+       title = " ") +
   geom_smooth(method = "lm", se = FALSE, linetype = "longdash", 
               size = 1.2, aes(color = dx), formula = y ~ x) +  # Ajuste lineal con colores diferenciados
   theme_minimal(base_size = 14) +  # Tamaño base de texto para mejor legibilidad
@@ -163,6 +176,59 @@ log_log_combined
 
 ggsave(filename = "combined_degree_dis_loglog.jpg",
        plot = log_log_combined,
+       width = 25,
+       height = 25,
+       units = "cm",
+       dpi = 300,
+)
+
+#Cummulative degree
+
+degree_distr_x.p <- ggplot(dis, aes(x = as.numeric(degree), y = as.numeric(CumulativeDegree), color = dx)) +
+  #geom_point(size = 1.5) +
+  geom_line(size = 2) +
+  #geom_smooth(method = "loess", se = FALSE, size = 1.5) +  # Suavizado LOESS para suavizar la línea
+  scale_x_continuous(limits = c(min(dis$degree), max(dis$degree)), breaks = seq(min(dis$degree), max(dis$degree), by = 15)) +  # Ajusta el eje X
+  labs(x = "Degree",
+       y = "Cumulative Degree", 
+       color = NULL) +  # Elimina el título de la leyenda
+  #scale_x_continuous(breaks = seq(1, max(dis$degree), by =4)) + # Adjust the x-axis to show only integer degrees
+  scale_color_manual(values = c("AD" = "#A3333D", "no AD" = "#477998")) + # Custom colors for AD and no AD
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +  # Rotate X-axis labels at 45 degrees
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1, size = 8),  # Tamaño reducido para etiquetas en el eje X
+    panel.grid.major = element_blank(),  # Elimina las líneas de la cuadrícula principal
+  )
+
+degree_distr_x.p
+
+#Save
+ggsave(filename = "accum_distr.jpg",
+       plot = degree_distr_x.p,
+       width = 50,
+       height = 25,
+       units = "cm",
+       dpi = 300,
+)
+
+#Grob
+
+library(cowplot)
+
+# Convertir el gráfico secundario a un objeto gráfico
+degree_inset <- ggdraw() +
+  draw_plot(log_log_combined) +  # Main plot
+  draw_plot(degree_distr_x.p, 
+            x = 0.4, y = 0.68, 
+            width = 0.6,
+            height = 0.3)  # Inset plot with positioning
+degree_inset
+
+#Save
+
+ggsave(filename = "degree_dis_inset.jpg",
+       plot = degree_inset,
        width = 25,
        height = 25,
        units = "cm",
@@ -239,34 +305,6 @@ k_logpk_noAD <-  ggplot(noADdegree_freq, aes(x = degree, y = Prob)) +
 
 k_pk_grid <- grid.arrange(k_logpk_AD, k_logpk_noAD, ncol =2)
 
-#Cummulative degree
-
-degree_distr_x.p <- ggplot(dis, aes(x = as.numeric(degree), y = as.numeric(CumulativeDegree), color = dx)) +
- #geom_point(size = 1.5) +
-  geom_line(size = 2) +
-  #geom_smooth(method = "loess", se = FALSE, size = 1.5) +  # Suavizado LOESS para suavizar la línea
-  scale_x_continuous(limits = c(min(dis$degree), max(dis$degree)), breaks = seq(min(dis$degree), max(dis$degree), by = 15)) +  # Ajusta el eje X
-  labs(subtitle = "b)", x = "Degree",
-       y = "Cumulative Degree") +
-  #scale_x_continuous(breaks = seq(1, max(dis$degree), by =4)) + # Adjust the x-axis to show only integer degrees
-  scale_color_manual(values = c("AD" = "#A3333D", "no AD" = "#477998")) + # Custom colors for AD and no AD
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +  # Rotate X-axis labels at 45 degrees
-  theme_minimal() +
-  theme(
-    panel.grid.major = element_blank(),  # Elimina las líneas de la cuadrícula principal
-    #panel.grid.minor = element_blank()   # Elimina las líneas de la cuadrícula secundaria
-  )
-
-degree_distr_x.p
-
-
-ggsave(filename = "accum_distr.jpg",
-       plot = degree_distr_x.p,
-       width = 50,
-       height = 25,
-       units = "cm",
-       dpi = 300,
-)
 
 
 #Final panel
@@ -280,5 +318,10 @@ ggsave(filename = "panel_loglog_accum_distr.jpg",
        units = "cm",
        dpi = 300,
 )
+
+#But now...
+
+
+
 
 #END
